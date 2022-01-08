423ef179bc3985d6c24b52d2af3512a9fed63913
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-01-07 21:13:08 -0500

    add click handler

--- src/shared/components/header/Button.tsx
+++ src/shared/components/header/Button.tsx
@@ -4,11 +4,12 @@ import Logo from './Logo';
 
 interface props {
   type: string;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function Button({ type }: props) {
+export default function Button({ type, onClick }: props) {
   return (
-    <button id={type} title={type}>
+    <button id={type} title={type} onClick={onClick}>
       <img src={`/img/icons/${type}.svg`} alt={type} draggable="false" />
     </button>
   );
