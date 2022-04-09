a64d9578455dddedef00ebc9ef85e8d21eac3633
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-04-09 17:10:41 -0400

    generalized css

--- src/options/components/App.tsx
+++ src/options/components/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.scss';
-import Header from './Header';
+import Header from '../../shared/components/header/Header';
 
 export default function App() {
   return (
     <div>
-      <Header></Header>
+      <Header fullscreen={false}></Header>
       <main></main>
     </div>
   );
