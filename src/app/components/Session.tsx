ea0b98ba5edda5d4f2acf79eb3f894377ffb7eea
blueputty01 <65861156+blueputty01@users.noreply.github.com>
2022-05-06 08:49:00 -0400

    add session base

---
+++ src/app/components/Session.tsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import Tab from "./Tab";
+import styles from "./Session.scss";
+
+export default function Session(props: WindowProps) {
+  return <div></div>;
+}
