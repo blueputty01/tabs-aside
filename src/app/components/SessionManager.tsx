fa61904f45be2fa149dc5eef31de5fd10f66591c
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-06-18 21:09:26 -0400

    remove errors

--- src/app/components/SessionManager.tsx
+++ src/app/components/SessionManager.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useChromeStorageLocal } from 'shared/utils/chrome.storage';
 import SessionCreation from './SessionCreation/SessionCreation';
 
 import Session from './Session';
-import { TabStore, SessionStore } from './Session';
-import { TabData } from 'shared/types/Tab';
+import { TabData, TabStore } from 'shared/types/Tab';
+import { SessionStore } from 'shared/types/Session';
 
 type SessionKeys = string[];
 
@@ -16,18 +16,22 @@ export default function SessionManager() {
 
     const [sessions, setSessions] = useState([] as SessionStore[]);
 
-  const saveSession = (title: string, checked: boolean, tabs: TabData[][]) => {
-    const ids: number[] = [];
-    const flatTabs = tabs.map((curr: TabData[]): TabStore[] => {
-      const lean = curr.map((tab: TabData): TabStore => {
-        ids.push(tab.id);
-        return { title: tab.title, url: tab.url };
-      });
-      return lean;
-    });
-    setSessions((prev: SessionStore[]): SessionStore[] => {
-      return [...prev, { title, tabs: flatTabs } as SessionStore];
-    });
+    const saveSession = (
+        title: string,
+        checked: boolean,
+        tabs: TabData[][]
+    ) => {
+        const ids: number[] = [];
+        const flatTabs = tabs.map((curr: TabData[]): TabStore[] => {
+            const lean = curr.map((tab: TabData): TabStore => {
+                ids.push(tab.id);
+                return { title: tab.title, url: tab.url };
+            });
+            return lean;
+        });
+        setSessions((prev: SessionStore[]): SessionStore[] => {
+            return [...prev, { title, tabs: flatTabs } as SessionStore];
+        });
 
         setKeys((prev: SessionStore[]): SessionStore[] => {
             return [...prev, { title, tabs: flatTabs } as SessionStore];
@@ -37,15 +41,6 @@ export default function SessionManager() {
             chrome.tabs.remove(ids);
         }
     };
-
-  const Sessions = sessionData.map((key: string) => (
-    <Session
-      deleteHandler={handler}
-      rightClickHandler={handler}
-      overflowClickHandler={handler}
-      key={key}
-    ></Session>
-  ));
 
     const Sessions = keys.map((key: string) => (
         <Session
